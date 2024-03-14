2fda95dbd980fce591c968ddc56a318950072d7c
RishiVerma2008 <91791943+RishiVerma2008@users.noreply.github.com>
2024-03-14 20:53:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 //Paste your firebaseConfig here
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDl0GHQWscr1PfOwpjwBL2Uap-iBQWqJ1U",
+  authDomain: "pro-71-fcab9.firebaseapp.com",
+  projectId: "pro-71-fcab9",
+  storageBucket: "pro-71-fcab9.appspot.com",
+  messagingSenderId: "513385878602",
+  appId: "1:513385878602:web:a775bea4f277bc9da802b3"
+};
 //
 firebase.initializeApp(firebaseConfig);
 
